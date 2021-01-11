e4b6a3e0026abff285e20c7ef4b4d6855ef0a1e8
Soňa Matějková <mat.sona53@gmail.com>
2021-01-11 20:00:45 +0100

    Z8klad hry

---
+++ script.js
@@ -0,0 +1,113 @@
+const width = 600;
+const height = 600;
+
+canvas.width = width;
+canvas.height = height;
+
+const ctx = canvas.getContext('2d');
+const blockSize = 30;
+
+let keys = [];
+
+const board = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+  [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+  [1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+  [1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+  [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1],
+  [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1],
+  [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+  [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1],
+  [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const wall = new Image();
+wall.src = 'images/wall.png';
+
+const generateBoard = () => {
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      if (board[y][x] === 1) {
+        ctx.drawImage(wall, x * blockSize, y * blockSize, blockSize, blockSize);
+      }
+    }
+  }
+};
+
+window.addEventListener('load', generateBoard);
+
+let player = {
+  x: 8,
+  y: 1,
+};
+
+const hero = new Image();
+hero.src = 'images/down.png';
+
+const draw = () => {
+  ctx.clearRect(
+    player.x * blockSize,
+    player.y * blockSize,
+    blockSize,
+    blockSize,
+  );
+  generateBoard();
+  ctx.drawImage(
+    hero,
+    player.x * blockSize,
+    player.y * blockSize,
+    blockSize,
+    blockSize,
+  );
+};
+
+window.addEventListener('load', draw);
+
+document.body.addEventListener('keydown', (e) => {
+  keys[e.code] = true;
+
+  draw();
+});
+
+document.body.addEventListener('keyup', (e) => {
+  keys[e.code] = false;
+
+  draw();
+});
+
+const movement = () => {
+  if (keys[39]) {
+    // šipka doprava
+    hero.src = 'images/right.png';
+    player.x++;
+  }
+
+  if (keys[37]) {
+    // šipka doleva
+    hero.src = 'images/left.png';
+    player.x--;
+  }
+
+  if (keys[38]) {
+    // šipka nahoru
+    hero.src = 'images/up.png';
+    player.y--;
+  }
+
+  if (keys[40]) {
+    // šipka dolů
+    hero.src = 'images/down.png';
+    player.y++;
+  }
+};
