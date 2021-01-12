9f7f0904ee31fe9d1a36bf5c870bcf08242b0264
Soňa Matějková <mat.sona53@gmail.com>
2021-01-12 21:28:39 +0100

    Časomíra a styly

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const width = 600;
 const height = 600;
+
+const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+
+canvas.width = width;
+canvas.height = height;
+
 const blockSize = 30;
-const keys = [];
-const player = {
+
+let keys = [];
+let pills = [];
+
+let player = {
   x: 8,
   y: 1,
 };
+
+const game = {
+  scoreElement: document.getElementById('score'),
+  score: 0,
+};
+
 const wall = new Image();
 wall.src = 'images/wall.png';
 
@@ -34,16 +49,14 @@ fruit2.src = 'images/fruit2.png';
 const tea = new Image();
 tea.src = 'images/tea.png';
 
-const pills = [];
-
 const scoreElement = document.getElementById('score');
 let score = 0;
 
 const timeElement = document.getElementById('time');
 let time = 0;
 
-canvas.width = width;
-canvas.height = height;
+const endElement = document.getElementById('end');
+let endMessage = document.getElementById('message');
 
 const board = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -67,80 +80,6 @@ const board = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
-
-const generateBoard = () => {
-  for (let y = 0; y < board.length; y++) {
-    for (let x = 0; x < board[y].length; x++) {
-      if (board[y][x] === 1) {
-        ctx.drawImage(wall, x * blockSize, y * blockSize, blockSize, blockSize);
-      }
-    }
-  }
-  for (let i = 0; i < pills.length; i++) {
-    ctx.drawImage(
-      pills[i].imageObject,
-      pills[i].x * blockSize,
-      pills[i].y * blockSize,
-      blockSize,
-      blockSize,
-    );
-  }
-};
-
-const canMove = (x, y) => {
-  return (
-    y >= 0 &&
-    y < board.length &&
-    x >= 0 &&
-    x < board[y].length &&
-    board[y][x] != 1
-  );
-};
-
-const movement = (e) => {
-  if (keys[38] && canMove(player.x + 1, player.y)) {
-    // šipka doprava
-    hero.src = 'images/right.png';
-    player.x++;
-  }
-
-  if (keys[40] && canMove(player.x - 1, player.y)) {
-    // šipka doleva
-    hero.src = 'images/left.png';
-    player.x--;
-  }
-
-  if (keys[37] && canMove(player.x, player.y - 1)) {
-    // šipka nahoru
-    hero.src = 'images/up.png';
-    player.y--;
-  }
-
-  if (keys[39] && canMove(player.x, player.y + 1)) {
-    // šipka dolů
-    hero.src = 'images/down.png';
-    player.y++;
-  }
-};
-
-const draw = (e) => {
-  ctx.clearRect(
-    player.x * blockSize,
-    player.y * blockSize,
-    blockSize,
-    blockSize,
-  );
-  generateBoard();
-  movement();
-  collect();
-  ctx.drawImage(
-    hero,
-    player.x * blockSize,
-    player.y * blockSize,
-    blockSize,
-    blockSize,
-  );
-};
 
 const createPills = () => {
   pills.push({
@@ -181,15 +120,61 @@ const createPills = () => {
 
   pills.push({
     x: 8,
-    y: 4,
+    y: 6,
     imageObject: tea,
   });
+};
+
+const generateBoard = () => {
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      if (board[y][x] === 1) {
+        ctx.drawImage(wall, x * blockSize, y * blockSize, blockSize, blockSize);
+      }
+    }
+  }
+  for (let i = 0; i < pills.length; i++) {
+    ctx.drawImage(
+      pills[i].imageObject,
+      pills[i].x * blockSize,
+      pills[i].y * blockSize,
+      blockSize,
+      blockSize,
+    );
+  }
 };
 
 const startGame = () => {
   time = 60;
   createPills();
   draw();
+  timer();
+};
+
+const movement = () => {
+  if (keys[39] && canMove(player.x + 1, player.y)) {
+    // šipka doprava
+    hero.src = 'images/right.png';
+    player.x++;
+  }
+
+  if (keys[37] && canMove(player.x - 1, player.y)) {
+    // šipka doleva
+    hero.src = 'images/left.png';
+    player.x--;
+  }
+
+  if (keys[38] && canMove(player.x, player.y - 1)) {
+    // šipka nahoru
+    hero.src = 'images/up.png';
+    player.y--;
+  }
+
+  if (keys[40] && canMove(player.x, player.y + 1)) {
+    // šipka dolů
+    hero.src = 'images/down.png';
+    player.y++;
+  }
 };
 
 const collect = () => {
@@ -203,7 +188,7 @@ const collect = () => {
 
 const increaseScore = () => {
   score++;
-  scoreElement.textContent = `${score}/6`;
+  scoreElement.textContent = `${score}/7`;
 };
 
 const timer = () => {
@@ -211,20 +196,86 @@ const timer = () => {
     let timer = time;
     let minutes = 0;
     let seconds = 0;
+
+    let countDownInterval = setInterval(() => {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+
+      //výhra
+      if (score === 7) {
+        clearInterval(countDownInterval);
+        endGame('win', timer);
+      }
+
+      //prohra
+      if (timer == 0) {
+        clearInterval(countDownInterval);
+        endGame('loss');
+      }
+
+      timeElement.textContent = minutes + ':' + seconds;
+
+      timer--;
+    }, 1000);
   };
   startTimer();
+};
+
+const endGame = (type, winTime) => {
+  if (type === 'win') {
+    endElement.style.display = 'block';
+    endMessage.textContent = `Vyhráli jste! Sesbírali jste všechny vitamíny za ${
+      time - winTime
+    } sekund.`;
+  }
+
+  if (type === 'loss') {
+    endElement.style.display = 'block';
+    endMessage.textContent = `Prohráli jste! Nestihli jste sesbírat všechny vitamíny. Zkuste to znovu.`;
+  }
+};
+
+const canMove = (x, y) => {
+  return (
+    y >= 0 &&
+    y < board.length &&
+    x >= 0 &&
+    x < board[y].length &&
+    board[y][x] != 1
+  );
+};
+
+const draw = () => {
+  ctx.clearRect(
+    player.x * blockSize,
+    player.y * blockSize,
+    blockSize,
+    blockSize,
+  );
+  generateBoard();
+  movement();
+  collect();
+  ctx.drawImage(
+    hero,
+    player.x * blockSize,
+    player.y * blockSize,
+    blockSize,
+    blockSize,
+  );
 };
 
 window.addEventListener('load', startGame);
 
 document.body.addEventListener('keydown', (e) => {
-  keys[e.code] = true;
-
+  keys[e.keyCode] = true;
   draw();
 });
 
 document.body.addEventListener('keyup', (e) => {
-  keys[e.code] = false;
+  keys[e.keyCode] = false;
 
   draw();
 });
